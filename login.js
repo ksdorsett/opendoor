d495e722a5fb42dbdfb4cfaff2f6742544eb921e
Kevin Dorsett <kevinsdorsett@gmail.com>
2017-04-18 15:16:58 -0400

    Started building login page

---
+++ login.js
@@ -0,0 +1,7 @@
+var onSubmit = function() {
+    var username = $('#uname').val();
+    var password = $('#psw').val();
+    if ((username!="")&&(password!="")){
+       alert("username is "+username+"\npassword is "+password);
+    }
+};
