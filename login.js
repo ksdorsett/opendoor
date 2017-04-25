12e5a00875b722914c2266413680219e232f809c
Kevin Dorsett <kevinsdorsett@gmail.com>
2017-04-25 15:42:06 -0400

    Unbroken carole and charley’s changes

--- login.js
+++ login.js
@@ -1,15 +1,7 @@
-(function () {
-    console.log('running...');
-    var main = function() {
-        $("button").on("click", function() {
-        console.log("clicked");
-        var username = $('#uname').val();
-        var password = $('#psw').val();
-        if ((username!="")&&(password!="")){
-            alert("username is "+username+"\npassword is "+password);
-        }
-        });
-    };
-    
-    $(document).ready(main());
-})();
+var onSubmit = function() {
+    var username = $('#uname').val();
+    var password = $('#psw').val();
+    if ((username!="")&&(password!="")){
+       alert("username is "+username+"\npassword is "+password);
+    }
+};
